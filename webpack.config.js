b9aadc9e06925fec939c7faf708197e0343ed50a
Joseph Blackmore <joeblackmore97@gmail.com>
2021-01-10 23:25:33 +0000

    Improve webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -7,9 +9,10 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.[contenthash].js',
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['*', '.js', '.jsx'],
   },
   module: {
     rules: [
@@ -18,18 +21,21 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
-        test: [/\.js$/, /\.jsx$/],
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
     ],
   },
   plugins: [
+    new ESLintPlugin(),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
     }),
   ],
   devServer: {
+    hot: true,
     open: true,
   },
 };
